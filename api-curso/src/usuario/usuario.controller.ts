5b3de12502846c873ac3f245380e356c750250bb
Cristian Lara <cristian89lara@gmail.com>
2023-05-08 19:54:37 -0600

    feat: controlador usaurio consumiendo servicio

--- api-curso/src/usuario/usuario.controller.ts
+++ api-curso/src/usuario/usuario.controller.ts
@@ -1,14 +1,20 @@
+import { UsuarioService } from './usuario.service';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('usuario')
 export class UsuarioController {
 
+    constructor(private readonly usuarioService: UsuarioService){}
 
-
-
-    @Get()
+    @Get('porId')
     getUsuario(){
 
-        return 'get usuario'
+        return this.usuarioService.getUsuario(1);
+    }
+
+    @Get('lista')
+    getListaUsuarios(){
+
+        return this.usuarioService.getAllUsuarios();
     }
 }
