4332d489446446e877486262ba114520180aecb9
Cristian Lara <cristian89lara@gmail.com>
2023-05-08 19:02:24 -0600

    feat: uso de variables de entorno

--- api-curso/src/app.controller.ts
+++ api-curso/src/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    console.log('variable de entorno ejemplo', process.env.EJEMPLO_VARIABLE)
+  console.log('nombre', process.env.NOMBRE)
+  console.log('edad', 14)
+  console.log('puerto base de datos', +process.env.BDD_PORT)
+  console.log('es prod', process.env.ES_PROD === 'false')
+  
+  }
 
   @Get()
   getHello(): string {
