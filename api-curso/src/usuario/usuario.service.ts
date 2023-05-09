fba0e90f1f2e5eb79a8b4ef2c24b7cb1882bbf09
Cristian Lara <cristian89lara@gmail.com>
2023-05-08 19:54:24 -0600

    feat: servicio usuario creado con metodos

--- api-curso/src/usuario/usuario.service.ts
+++ api-curso/src/usuario/usuario.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsuarioService {}
+export class UsuarioService {
+    usuarios = [
+        {
+            id: 1,
+            usuario: 'Cristian',
+            password: '1234'
+        },
+        {
+            id: 2,
+            usuario: 'user',
+            password: '1234'
+        },
+        {
+            id: 3,
+            usuario: 'user test',
+            password: '1234'
+        }
+    ]
+
+    getUsuario(idUsuario:number){
+        return this.usuarios.find(usuario => usuario.id === idUsuario)
+    }
+
+    getAllUsuarios(){
+        return this.usuarios;
+    }
+}
